dcc810419e5302543175dc65dff0523bc8898067
Vasco Silveira <vasco.silveira@saltpay.co>
2022-02-19 16:40:07 +0000

    added yaser's validations and associations to courses and institutions

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,3 +1,6 @@
 class Institution < ApplicationRecord
-  has_many :course
+  has_many :courses
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
 end
