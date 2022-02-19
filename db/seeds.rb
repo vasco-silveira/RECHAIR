2572446a75e69aaceeced18ff833b79795acd3d8
Vasco Silveira <vasco.silveira@saltpay.co>
2022-02-19 16:32:25 +0000

    Created seeds, removed course references in institutions table, and altered validations in course and institution models (might generate conflicts)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,33 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-# 10.times do
-#   User.create(
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name,
-#     address: Faker::Address.street_address,
-#     email: Faker::Internet.email,
-#     password: '123456789'
-#   )
-# end
+10.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: '123456789'
+  )
+end
 
-# 5.times do
-#     Institution.create(
-#       first_name: Faker::Name.first_name,
-#       last_name: Faker::Name.last_name,
-#       address: Faker::Address.street_address,
-#       email: Faker::Internet.email,
-#       password: '123456789'
-#     )
-#   end
+10.times do
+    Institution.create!(
+      name: Faker::University.name,
+      address: Faker::Address.street_address,
+      description: Faker::Lorem.paragraphs(number: 1),
+      image_url: Faker::LoremFlickr.image
+    )
+  end
 
-# 25.times do
-#   Game.create(
-#     title: Faker::Game.title,
-#     description: Faker::Lorem.paragraphs(number: 1),
-#     image_url: Faker::LoremFlickr.image,
-#     user_id: rand(2..15),
-#     condition: ['good', 'fair', 'poor'].sample
-#   )
-# end
+40.times do
+  Course.create(
+    professor: Faker::Name.name,
+    title: Faker::Educator.course_name,
+    description: Faker::Lorem.paragraphs(number: 1),
+    subject: Faker::Educator.subject,
+    price: Faker::Number.number(digits: 3),
+    start_date: Faker::Date.in_date_period,
+    end_date: Faker::Date.in_date_period(year: 2023, month: 6),
+    rating: [1, 2, 3, 4, 5].sample,
+    image_url: Faker::LoremFlickr.image,
+    institution_id: rand(2..10)
+  )
+end
