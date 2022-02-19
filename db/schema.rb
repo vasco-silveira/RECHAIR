4ded83f4b7961aa56a0bd7c17c354642a9c6b7e9
Vasco Silveira <vasco.silveira@saltpay.co>
2022-02-19 10:58:46 +0000

    ran db:migrate on for Course model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_02_19_105042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "professor"
+    t.string "title"
+    t.text "description"
+    t.string "subject"
+    t.integer "price"
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
