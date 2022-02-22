dd26850e02aa609467d5d7a7e44781efeb3e533d
Yaser Hussain <yaserhussain5@gmail.com>
2022-02-22 19:23:15 +0000

    made some amendments to validations so seed file can work

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,6 +7,16 @@ class Course < ApplicationRecord
   validates :description, presence: true, length: { minimum: 10 }
   # validates :subject, inclusion: { in: %w(Art and Design Business & Economics Computer Science Education Engineering Geography History Languages Law Medicine Philosophy Politics and International Relations Sciences Sport Studies), message: "Please choose a subject"}
   validates :price, presence: true, numericality: { only_integer: true }
-  validates :start_date, presence: true
-  validates :end_date, presence: true, date: { after_or_equal_to: :start_date }
+  validates :start_date, :end_date, presence: true
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
 end
