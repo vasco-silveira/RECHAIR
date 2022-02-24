504e72a600b22f52904bd5dc61238fed23dc4cad
Yaser Hussain <yaserhussain5@gmail.com>
2022-02-24 21:06:00 +0000

    bookings controller kind of complete but may need to make some changes i don't know. BUT ... it's there. also made one change to course page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,50 @@
 class BookingsController < ApplicationController
-    # IGNORE: git Adding comment to check conflicts.
+  before_action :set_booking, only: [ :show, :destroy ]
+  before_action :set_user
+  # Need to make sure this displays bookings for the current user only
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @course = Course.find(params[:course_id])
+    @booking.user = current_user
+    @booking.course = @course
+    @course.save
+    if @course.save!
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:course_id, :user_id)
+  end
+
+  def set_user
+    if user_signed_in?
+      @user = current_user
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -59,8 +59,21 @@ ActiveRecord::Schema.define(version: 2022_02_19_160458) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "views", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_views_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "courses"
