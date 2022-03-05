c7865796ffe0e5328b89786f3f9ae492e48c9b5d
Yaser Hussain <yaserhussain5@gmail.com>
2022-03-05 17:06:00 +0000

    confirmation pop up - unfinished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -40,29 +40,26 @@ document.addEventListener('turbolinks:load', () => {
         showCancelButton: true,
         cancelButtonColor: '#d33',
         confirmButtonColor: '#3085d6',
-        confirmButtonText: 'confirm'
+        confirmButtonText: 'confirm',
       }).then((result) => {
         if (result.isConfirmed) {
-          // Swal.fire(
-          //   'Chair confirmed!',
-          //   "You're all set! You'll receive an email from your institution confirming your chair and further details on your chosen course. To manage your course, head over to your profile page",
-          //   'success'
-          // )
-
           const userId = reservationBtn.dataset.userId;
           const courseId = reservationBtn.dataset.courseId;
-          const data = {user_id: userId, course_id: courseId};
+          const data = { user_id: userId, course_id: courseId };
 
           fetch(`/courses/${courseId}/bookings`, {
             method: "POST",
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
           }).then(res => {
             console.log("Request complete! response:", res);
-
+            Swal.fire(
+              'Chair confirmed!',
+              "You're all set! You'll receive an email from your institution confirming your chair and further details on your chosen course. To manage your course, head over to your profile page",
+              'success'
+            )
+            // window.location.href = `/courses/${courseId}`;
           });
-          // window.location = `./courses/${courseId}`;
-          reload();
         }
       })
     });
