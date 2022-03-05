ac06af715e4c79aca40768097b597f92c4f2e1de
Vasco Silveira <vasco.silveira@saltpay.co>
2022-03-05 14:48:09 +0000

    chatroom controller and model, migrations to have nickname in users and chatroom and message, created new routes for chatroom and show page for chatroom general

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_201932) do
+ActiveRecord::Schema.define(version: 2022_03_05_144129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2022_03_01_201932) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["course_id"], name: "index_bookings_on_course_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "courses", force: :cascade do |t|
@@ -53,6 +59,16 @@ ActiveRecord::Schema.define(version: 2022_03_01_201932) do
     t.string "zipcode"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -65,6 +81,7 @@ ActiveRecord::Schema.define(version: 2022_03_01_201932) do
     t.string "last_name"
     t.string "type"
     t.boolean "admin", default: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -72,4 +89,6 @@ ActiveRecord::Schema.define(version: 2022_03_01_201932) do
   add_foreign_key "bookings", "courses"
   add_foreign_key "bookings", "users"
   add_foreign_key "courses", "institutions"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end
