00101a332eb299a4c5926c7fbd1303ece9519b0a
Yaser Hussain <yaserhussain5@gmail.com>
2022-03-05 15:26:06 +0000

    attempts to make a confirmation pop up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
+import Swal from 'sweetalert2'
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -27,4 +28,44 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  const reservationBtn = document.getElementById("reservation-btn");
+  if (reservationBtn) {
+    console.log("reservation available");
+    reservationBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      Swal.fire({
+        title: 'Confirm Chair',
+        text: 'Do you want to continue',
+        icon: 'warning',
+        showCancelButton: true,
+        cancelButtonColor: '#d33',
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'confirm'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // Swal.fire(
+          //   'Chair confirmed!',
+          //   "You're all set! You'll receive an email from your institution confirming your chair and further details on your chosen course. To manage your course, head over to your profile page",
+          //   'success'
+          // )
+
+          const userId = reservationBtn.dataset.userId;
+          const courseId = reservationBtn.dataset.courseId;
+          const data = {user_id: userId, course_id: courseId};
+
+          fetch(`/courses/${courseId}/bookings`, {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+          }).then(res => {
+            console.log("Request complete! response:", res);
+
+          });
+          // window.location = `./courses/${courseId}`;
+          reload();
+        }
+      })
+    });
+  }
+
 });
