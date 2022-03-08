086d2601c918b53626809dea81b3406ad5634d00
Alex Davies <alxdavies@googlemail.com>
2022-03-08 19:47:53 +0000

    button added to index page search

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_03_05_111042) do
 ActiveRecord::Schema.define(version: 2022_03_05_144129) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
