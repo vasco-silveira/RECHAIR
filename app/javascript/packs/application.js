f7f2ca2e26bcaf7144eac82470a5df2ab1d28a10
Shotiko Abramishvili <shotikoabrama@gmail.com>
2022-03-08 21:11:08 +0000

    fixed SIGN OUT

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,48 +24,12 @@ import Swal from 'sweetalert2'
 
 // Internal imports, e.g:
 import { initChatroomCable } from '../channels/chatroom_channel';
-
+import {successpopup} from '../components/successpopup';
 document.addEventListener('turbolinks:load', () => {
 
   // Call your functions here, e.g:
+
   // initSelect2();
-  const reservationBtn = document.getElementById("reservation-btn");
-  if (reservationBtn) {
-    console.log("reservation available");
-    reservationBtn.addEventListener("click", (event) => {
-      event.preventDefault();
-      Swal.fire({
-        title: 'Confirm Chair',
-        text: 'Do you want to continue',
-        icon: 'warning',
-        showCancelButton: true,
-        cancelButtonColor: '#d33',
-        confirmButtonColor: '#3085d6',
-        confirmButtonText: 'confirm',
-      }).then((result) => {
-        if (result.isConfirmed) {
-          const userId = reservationBtn.dataset.userId;
-          const courseId = reservationBtn.dataset.courseId;
-          const data = { user_id: userId, course_id: courseId };
-
-          fetch(`/courses/${courseId}/bookings`, {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-          }).then(res => {
-            console.log("Request complete! response:", res);
-            Swal.fire(
-              'Chair confirmed!',
-              "You're all set! You'll receive an email from your institution confirming your chair and further details on your chosen course. To manage your course, head over to your profile page",
-              'success'
-            )
-            // window.location.href = `/courses/${courseId}`;
-          });
-        }
-      })
-    });
-  }
-
+  successpopup()
   initChatroomCable();
-
 });
