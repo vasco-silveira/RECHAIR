d57336476d5eb19cc375f1bf0618cd9be0759bd1
Vasco Silveira <vasco.silveira@saltpay.co>
2022-03-10 21:09:47 +0000

    Added new and create functions to chatrooms controller and nested chatroom routes inside courses routes

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,33 @@
 class ChatroomsController < ApplicationController
+  before_action :set_user
+  
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+  end
+
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new
+    @course = Course.find(params[:course_id])
+    @chatroom.user = current_user
+    @chatroom.course = @course
+    @chatroom.save
+    if @chatroom.save!
+      redirect_to chatrooms_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_user
+    if user_signed_in?
+      @user = current_user
+    end
   end
 end
